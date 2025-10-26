fccf9aa36e3f1db7ac0c95168e47b5393731313c
FawzyBashandy <fawzybashandy@gmail.com>
2025-10-26 13:45:44 +0300

    Adding the Script

---
+++ FawzyScript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~
+mkdir myDirectory
+mkdir myDirectory/secondDirectory
+touch myDirectory/secondDirectory/myNotePaper
+cp myDirectory/secondDirectory/myNotePaper myDirectory/
+mv myDirectory/myNotePaper myDirectory/myOldNotePaper
+
